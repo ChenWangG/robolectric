38a5fe3645daa0207aff6c9d219b63de2c053b84
Chen Wang <ChenWangG@google.com>
2026-04-10 21:34:04 +0000

    Add robolectric test for L2CAP.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# robolectric
-Test the open-sourced robolectric from https://github.com/robolectric/robolectric.
+Used to test the open-sourced robolectric from
+https://github.com/robolectric/robolectric.
+
+After clone the repo, you need to add a file local.properties
+under the same directory of this README with a line as bleow.
+
+sdk.dir=/Users/Name/Library/Android/sdk
+
+The value of sdk.dir shoud be a valid Android SDK of your local machine,
+which can be downloaded from
+https://developer.android.com/tools/releases/platform-tools#downloads
---
+++ l2cap/src/test/java/com/google/l2cap/L2CapTest.java
@@ -0,0 +1,22 @@
+package com.google.l2cap;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import java.io.IOException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Robolectric test. */
+@RunWith(AndroidJUnit4.class)
+public final class L2CapTest {
+  @Test
+  public void testL2cap() throws IOException {
+    Context context = ApplicationProvider.getApplicationContext();
+    BluetoothAdapter adapter = context.getSystemService(BluetoothManager.class).getAdapter();
+    // This will incur java.lang.NoSuchMethodError.
+    // adapter.listenUsingInsecureL2capChannel();
+  }
+}
